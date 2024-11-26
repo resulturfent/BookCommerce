c034019edd375dc4b6d502ae9712e88466a106c9
resulturfent <rsltrfnt@gmail.com>
2024-11-26 12:39:11 +0300

    Sepet Index template eklendi

---
+++ E_Book_Commerce/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Commerce.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult CartIndex()
+        {
+            return View();
+        }
+    }
+}
